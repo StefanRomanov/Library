afe27168c571673ae90a523ee0c7e5c2b30e94ce
stefanromanov <romanovsc2@gmail.com>
2019-09-26 00:38:54 +0300

    Added readme.md

---
+++ server/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>Supported routes:</h1>
+<ul>
+    <li><h3>GET /api/books => Get all books</h3></li>
+    <li><h3>POST /api/books => Add new book</h3>/li>
+    <li><h3>PUT /api/books/:bookId => Edit book </h3></li>
+    <li><h3>DELETE /api/books/:bookId => Delete book </h3></li>
+</ul>
+</body>
+</html>
